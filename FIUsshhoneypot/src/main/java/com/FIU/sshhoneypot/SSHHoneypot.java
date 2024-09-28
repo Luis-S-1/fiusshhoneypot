d0e9118b7858e13a9a97809839161a0a0729c2bc
Luis Suarez <lsuar137@fiu.edu>
2024-09-27 22:16:54 -0400

    Added student header comment

--- FIUsshhoneypot/src/main/java/com/FIU/sshhoneypot/SSHHoneypot.java
+++ FIUsshhoneypot/src/main/java/com/FIU/sshhoneypot/SSHHoneypot.java
@@ -1,3 +1,16 @@
+/*********************************************************************
+ Authors   : Abdulla Al-Naimi, Darrick Sanders, John Espinal, Manuela Calle, Luis Suarez
+ Course    : CIS3950 Capstone I
+ Professor : Masoud Sadjadi 
+ Program Purpose/Description
+Our project idea is to create an SSH Honeypot. SSH Honeypot is a network decoy deployed temporarily on port 22 
+and is used to capture any data on intruders trying to connect to our network through a remote connection. 
+Data includes any credentials used to log in, IP addresses, and any activity after login.
+
+ Due Date  : 12/07/2024 
+ 
+*********************************************************************/
+
 package com.FIU.sshhoneypot;
 
 import org.apache.sshd.server.SshServer;
